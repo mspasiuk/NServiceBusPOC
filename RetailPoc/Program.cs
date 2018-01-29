f7637804c26a8cbd47b2cb363a8246ec55aa04d0
mspasiuk <mspasiuk@gmail.com>
2018-01-29 19:46:19 -0300

    Add project files.

---
+++ RetailPoc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RetailPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
